bfd23fcbf54757dd2f63187714ece1c6aa90525e
Jon Russell <jrussell@nodeomega.com>
2014-12-22 00:12:18 -0800

    Initial commit.

---
+++ AstroGearsDurandal/App_Start/BundleConfig.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BundleConfig.cs" company="Jonathan Russell">
+//   Copyright (c) Jonathan Russell.  All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the BundleConfig type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AstroGearsDurandal
+{
+    using System.Web.Optimization;
+
+    /// <summary>
+    /// Defines the BundleConfig.
+    /// </summary>
+    public class BundleConfig
+    {
+        /// <summary>
+        /// Registers the bundles.
+        /// </summary>
+        /// <param name="bundles">The bundles.</param>
+        /// <remarks>
+        /// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        /// </remarks>
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ AstroGearsDurandal/App_Start/DurandalConfig.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DurandalConfig.cs" company="Jonathan Russell">
+//   Copyright (c) Jonathan Russell.  All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the DurandalConfig type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+////[assembly: WebActivator.PostApplicationStartMethod(
+////    typeof(BetterDurandalTest.App_Start.DurandalConfig), "PreStart")]
+
+namespace AstroGearsDurandal
+{
+    using System.Web.Optimization;
+
+    /// <summary>
+    /// Defines the DurandalConfig type.
+    /// </summary>
+    public static class DurandalConfig
+    {
+        /// <summary>
+        /// Pre starts the application.
+        /// </summary>
+        public static void PreStart()
+        {
+            // Add your start logic here
+            DurandalBundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
